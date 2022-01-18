6cb0efb7c0d19ff6aa18c4ef4f268a549d6d86e4
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-01-18 21:45:07 +0900

    relate React with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+//import logo from './logo.svg';
 import './App.css';
-import { TestComponent } from './TestComponent';
+import  { db } from "./firebase";
+//import { TestComponent } from './TestComponent';
 
-// function App() {
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState([{id:"", title:""}]);
+  useEffect(() => {
+    const unSub = db.collection("tasks").onSnapshot((snapshot)=>{
+      setTasks(
+        snapshot.docs.map((doc)=> ({id: doc.id, title: doc.data().title}))
+      );
+    });
+    return ()=> unSub();
+
+  }, []);
+
+  return(
+    <div className="App">
+      {tasks.map((task)=><h3>{task.title}</h3>
+      )}
+    </div>
+  );
+};
+
+// const App:React.FC = () => {
 //   return (
 //     <div className="App">
 //       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
+//         <TestComponent text="hello from App"/>
 //       </header>
 //     </div>
-//   );
+//   )
 // }
-
-const App:React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <TestComponent text="hello from App"/>
-      </header>
-    </div>
-  )
-}
 
 export default App;
