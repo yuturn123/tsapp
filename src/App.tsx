1d1a5e7f273734545459efa59defe02aebed26d2
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-01-19 23:50:02 +0900

    make authentication func

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import { FormControl, List, TextField } from '@material-ui/core';
 import React, { useState, useEffect } from 'react';
 //import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 import  { db } from "./firebase";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
+import TaskItem from './TaskItem';
+import { makeStyles } from '@material-ui/core';
 //import { TestComponent } from './TestComponent';
 
-const App: React.FC = () => {
+import { auth } from "./firebase";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+const useStyles = makeStyles({
+  field:{
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list:{
+    margin: "auto",
+    width: "40%",
+  }
+});
+
+const App: React.FC = (props: any) => {
   const [tasks, setTasks] = useState([{id:"", title:""}]);
+  const [input, setInput] = useState("");
+  const classes = useStyles();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((user)=>{
+      !user && props.history.push("login")
+    });
+    return ()=> unSub();
+  })
+
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot)=>{
       setTasks(
@@ -16,14 +44,54 @@ const App: React.FC = () => {
 
   }, []);
 
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>)=>{
+    db.collection("tasks").add({title: input});
+    setInput("");
+  }
+
   return(
-    <div className="App">
-      {tasks.map((task)=><h3>{task.title}</h3>
-      )}
+    <div className={styles.app__root}>
+    <h1>Todo App by React/Firebase</h1>
+    <button className={styles.app__logout}
+    onClick={
+      async ()=>{
+        try{await auth.signOut();
+            props.history.push("login");
+        }catch(error: any){
+          alert(error.message);
+        }
+      }
+    }
+    >
+      {" "}
+      <ExitToAppIcon />
+    </button>
+
+    <br />
+    <FormControl>
+      <TextField
+      className={classes.field}
+      label="New task ?"
+      value={input}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>)=>setInput(e.target.value)}
+      />
+    </FormControl>
+    <button className={styles.app__icon}disabled={!input} onClick={newTask}>
+      <AddToPhotosIcon />
+    </button>
+
+
+      <List className={classes.list}>
+        {tasks.map((task)=>(
+          <TaskItem key={task.id} id={task.id} title={task.title}/>
+
+        ))}
+      </List>
     </div>
   );
 };
 
+// //For Test
 // const App:React.FC = () => {
 //   return (
 //     <div className="App">
